47f9ea322204de04e8129c1c2dc3131275aafb16
Daniel Perez Alvarez <unindented@gmail.com>
2017-03-23 20:39:21 -0700

    Exercise 1.4

--- src/chapter-1.scm
+++ src/chapter-1.scm
@@ -18,17 +18,29 @@
 ;; Define a procedure that takes three numbers as arguments and returns the sum
 ;; of the squares of the two larger numbers.
 (define (sum-of-squared-largest-two x y z)
-  (define (square x) (* x x))
+  (define (square x)
+    (* x x))
 
-  (define (sum-of-squares x y) (+ (square x) (square y)))
+  (define (sum-of-squares x y)
+    (+ (square x) (square y)))
 
-  (cond ((= (min x y z) x) (sum-of-squares y z))
-        ((= (min x y z) y) (sum-of-squares x z))
-        ((= (min x y z) z) (sum-of-squares x y))))
+  (if (>= x y)
+    (sum-of-squares x (if (>= y z) y z))
+    (sum-of-squares y (if (>= x z) x z))))
 
 (test-eqv 25 (sum-of-squared-largest-two 2 3 4))
 (test-eqv 25 (sum-of-squared-largest-two 3 4 2))
 (test-eqv 25 (sum-of-squared-largest-two 4 2 3))
 (test-end "Exercise 1.3")
+
+(test-begin "Exercise 1.4")
+;; Our model of evaluation allows for combinations whose operators are compound
+;; expressions.
+(define (a-plus-abs-b a b)
+  ((if (> b 0) + -) a b))
+
+(test-eqv 5 (a-plus-abs-b 3 2))
+(test-eqv 5 (a-plus-abs-b 3 -2))
+(test-end "Exercise 1.4")
 
 (test-end "Chapter 1")
